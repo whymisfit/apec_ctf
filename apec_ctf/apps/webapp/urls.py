fc68aa0f8e52a043d168575cf9c07339d06d20ab
Sergey Volchkov <debfantasys@gmail.com>
2020-09-28 19:19:19 +0700

    update style and base_webapp, rating_page

--- apec_ctf/apps/webapp/urls.py
+++ apec_ctf/apps/webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = (
     path('', views.TasksPage.as_view(), name='tasks_page'),
-]
+    path('rating/', views.RatingPage.as_view(), name='rating_page'),
+)
--- apec_ctf/apps/webapp/views.py
+++ apec_ctf/apps/webapp/views.py
@@ -5,7 +5,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class TasksPage(View):
     login_url = '/signin/'
-    template_name = 'webapp/tasks_page.html'
 
     def get(self, request):
-        return render(request, self.template_name,)
+        return render(request, 'webapp/pages/tasks_page.html',)
+
+
+class RatingPage(View):
+    login_url = '/signin/'
+
+    def get(self, request):
+        return render(request, 'webapp/pages/rating_page.html')
