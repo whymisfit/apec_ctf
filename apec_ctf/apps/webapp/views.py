c3743a793e7586d47ef65ebfe770c84222c6aa0d
Sergey Volchkov <debfantasys@gmail.com>
2020-09-28 12:13:07 +0700

    added base_webapp and tasks_page. update style.css

--- apec_ctf/apps/webapp/views.py
+++ apec_ctf/apps/webapp/views.py
@@ -3,9 +3,9 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TasksPage(LoginRequiredMixin, View):
+class TasksPage(View):
     login_url = '/signin/'
     template_name = 'webapp/tasks_page.html'
 
     def get(self, request):
-        return render(request, self.template_name, )
+        return render(request, self.template_name,)
