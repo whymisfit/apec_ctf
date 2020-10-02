e6102f0fcbdbe315b6851a34bea3150347b825c0
Sergey Volchkov <debfantasys@gmail.com>
2020-10-02 21:35:36 +0700

    update project structure. update rating page. register new model.

--- apec_ctf/apps/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = (
-    path('', views.TaskListPage.as_view(), name='tasks_page'),
-    path('rating/', views.RatingPage.as_view(), name='rating_page'),
-    path('task/<task_id>', views.TaskPage.as_view(), name='task_page'),
-)
